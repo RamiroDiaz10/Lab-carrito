09b11a2a3b4b6c76b4dba9c27a6649a04a4795b3
Ramiro Diaz <jramiroda10@gmail.com>
2026-06-12 15:17:06 -0500

    [+]add style.css: estilos para toda la pagina

--- app.js
+++ app.js
@@ -1,22 +1,40 @@
 const buttons = document.querySelectorAll(".btn-agregar");
-const listaCarrito = document.getElementById("#lista-carrito");
-const badge = document.getElementById("#badge");
-const total = document.getElementById("#total");
+const listaCarrito = document.getElementById("lista-carrito");
+const badge = document.getElementById("badge");
+const total = document.getElementById("total");
+const btnVaciar = document.getElementById("btn-vaciar");
+const msgVacio = document.getElementById("msg-vacio");
 
 
 let cantidadItems = 0;
 let totalCarrito = 0;
 
+btnVaciar.addEventListener("click", () =>{
+    listaCarrito.querySelectorAll("li").forEach(li=>{li.remove()});
+
+    cantidadItems = 0;
+    totalCarrito = 0;
+
+    updateBadge();
+    updateTotal();
+
+     msgVacio.style.display = "block";
+});
+
 buttons.forEach( button => {
     button.addEventListener("click", () =>{
         const nombre = button.dataset.nombre;
-        const precio = button.dataset.precio;
+        const precio = Number(button.dataset.precio);
 
         agregarAlCarrito(nombre, precio);
     })
 });
 
 function agregarAlCarrito(nombre, precio){
+    precio = Number(precio);
+
+    msgVacio.style.display = "none";
+
     let li = document.createElement("li");
 
     li.innerHTML = `
@@ -27,11 +45,13 @@ function agregarAlCarrito(nombre, precio){
     listaCarrito.appendChild(li);
 
     cantidadItems++
+    totalCarrito += precio;
+
     updateBadge();
     updateTotal();
 
-    const btnEliminar = li.querySelector("btn-eliminar");
-    btnEliminar.addEventListener("click", ()=>{
+    const btnEliminar = li.querySelector(".btn-eliminar");
+    btnEliminar.addEventListener("click", ()=> {
         eliminarItem(li,precio);
     });
 }
@@ -54,4 +74,5 @@ function eliminarItem(li, precio){
 function updateTotal (){
     total.textContent = "$" + totalCarrito.toLocaleString("es-CO", {minimumFractionDigits: 2});   //para formato regional y pesos
 }
+
 
